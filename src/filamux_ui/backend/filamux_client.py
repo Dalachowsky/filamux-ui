6de158088bb1b5f6e32db06e3be16959e6d43b7e
Kacper Dalach <dalachowsky@gmail.com>
2024-05-24 23:16:15 +0200

    comm: Initial serial port

--- src/filamux_ui/backend/filamux_client.py
+++ src/filamux_ui/backend/filamux_client.py
@@ -1,13 +1,15 @@
 
 from PySide6.QtSerialPort import QSerialPort
-from PySide6.QtCore import QIODevice
+from PySide6.QtCore import QIODevice, Slot, Signal
 from .protobuf import *
+from crc import Calculator, Crc16
 
 class FilamuxClient:
 
     def __init__(self):
         self._connected = False
         self._port = QSerialPort()
+        self._transactionOngoing = False
 
     def isConnected(self):
         return self._port.isConnected()
@@ -21,5 +23,29 @@ class FilamuxClient:
     def start(self):
         self._port.open(QIODevice.OpenModeFlag.ReadWrite)
 
+    def process(self):
+        pass
+
     def stop(self):
         self._port.close()
+
+    def _sendData(self, messageType: MessageType, data: bytearray):
+        frame = bytearray()
+        frame.append(0x7E)
+        frame.append(messageType)
+        frame.append(data.count())
+        frame.append(data)
+
+        calc = Calculator(Crc16.KERMIT)
+        crc = calc.checksum(data)
+
+        frame.append((crc>>8) & 0xFF)
+        frame.append(crc & 0xFF)
+
+        self._port.write(frame)
+
+    @Slot(int)
+    def sendSetActiveSpool(self, spool: int):
+        req = SetTargetSpoolReq(index=spool)
+        data = req.SerializeToString()
+        self._sendData(MessageType.MSG_SET_TARGET_SPOOL, data)
---
+++ src/filamux_ui/backend/protobuf/__init__.py
@@ -0,0 +1,3 @@
+
+from .common_pb2 import MessageType
+from .set_target_spool_pb2 import SetTargetSpoolReq, SetTargetSpoolRes
