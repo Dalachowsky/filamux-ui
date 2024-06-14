6bfae075f23cb80fcff8af1f99f57209dc23624f
Kacper Dalach <dalachowsky@gmail.com>
2024-06-14 18:25:13 +0200

    pb: imports

--- src/filamux_ui/backend/protobuf/__init__.py
+++ src/filamux_ui/backend/protobuf/__init__.py
@@ -1,3 +1,7 @@
 
 from .common_pb2 import MessageType
+from .extruder_gcode_pb2 import ExtruderGCodeReq, ExtruderGCodeRes
+from .get_spool_params_pb2 import GetSpoolParamsReq, GetSpoolParamsRes
+from .get_status_pb2 import GetStatusReq, GetStatusRes
+from .set_spool_params_pb2 import SetSpoolParamsReq, SetSpoolParamsRes
 from .set_target_spool_pb2 import SetTargetSpoolReq, SetTargetSpoolRes
